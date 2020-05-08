12423ea37cbc819507a8de4250db6924c3583faf
Enrique Prediger <henry@lamachina.local>
2020-05-08 17:55:20 +0100

    Connection pool monitoring for Golang sample app

--- apps/golang/main.go
+++ apps/golang/main.go
@@ -7,105 +7,66 @@ import (
 	"os"
 	"time"
 
+	"github.com/kr/pretty"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type MyAggResult struct {
-	Id         string   `bson:"_id"`
-	TotalTips  float64  `bson:"total_tips"`
-	PickupArea []string `bson:"pickup_area"`
-}
-
-func randomInsert()
-
 func main() {
+
 	mongoURI := os.Getenv("URI")
 
 	if len(mongoURI) == 0 {
 		panic("No URI")
 	}
 
-	iteration := 0
+	poolMonitor := &event.PoolMonitor{
+		Event: func(evt *event.PoolEvent) {
+			log.Printf("%# v", pretty.Formatter(evt))
+		},
+	}
+
+	// Don't set the Command monitor if it gets too verbose for the CRUD operations
+	/*cmdMonitor := &event.CommandMonitor{
+		Started: func(_ context.Context, evt *event.CommandStartedEvent) {
+			log.Printf("%# v", pretty.Formatter(evt))
+		},
+		Succeeded: func(_ context.Context, evt *event.CommandSucceededEvent) {
+			log.Printf("%# v", pretty.Formatter(evt))
+		},
+		Failed: func(_ context.Context, evt *event.CommandFailedEvent) {
+			log.Printf("%# v", pretty.Formatter(evt))
+		},
+	}*/
+
+	clientOptions := options.Client().ApplyURI(mongoURI).SetPoolMonitor(poolMonitor) //.SetMonitor(cmdMonitor)
+
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	database := client.Database("golang")
+	collection := database.Collection("demo")
+
+	log.Println("Dropping the sample collection")
+	_ = database.RunCommand(
+		context.Background(),
+		bson.D{{"drop", "demo"}},
+	)
 
 	for {
-
-		client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("Iteration %v\n", iteration)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		defer cancel()
-		err = client.Connect(ctx)
-		if err != nil {
-			panic(err)
-		}
-
-		database := client.Database("golang")
-		collection := database.Collection("tour")
-
-		log.Println("Dropping collection golang.tour (command)")
-		result := database.RunCommand(
-			context.Background(),
-			bson.D{{"drop", "tour"}},
-		)
-		fmt.Println(result)
-
-		fmt.Println("Inserting a single document.")
-		res1, err := collection.InsertOne(
-			context.Background(),
-			bson.D{
-				{"name", "MongoDB"},
-				{"type", "database"},
-				{"count", 1},
-				{"tags", bson.A{"webscale"}},
-				{"info", bson.D{
-					{"x", 203},
-					{"y", 102},
-					{"z", "N/A"},
-				}},
-			})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		id := res1.InsertedID
-		fmt.Println(id)
-
-		fmt.Println("Inserting multiple documents.")
-		_, err = collection.InsertMany(
-			context.Background(),
-			[]interface{}{
-				bson.D{
-					{"item", "journal"},
-					{"qty", 25},
-					{"tags", bson.A{"blank", "red"}},
-					{"size", bson.D{
-						{"h", 14},
-						{"w", 21},
-						{"uom", "cm"},
-					}},
-				},
-				bson.D{
-					{"item", "mat"},
-					{"qty", 25},
-					{"tags", bson.A{"gray"}},
-					{"size", bson.D{
-						{"h", 27.9},
-						{"w", 35.5},
-						{"uom", "cm"},
-					}},
-				},
-			})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Iterating over a collection with Find()")
+		_, err = collection.InsertOne(ctx, bson.D{{"x", 1}})
+		time.Sleep(1 * time.Second)
 		cur, err := collection.Find(context.Background(), bson.D{})
 		if err != nil {
 			log.Fatal(err)
@@ -118,40 +79,13 @@ func main() {
 				fmt.Println("Failed to decode bytes")
 				log.Fatal(err)
 			}
-			fmt.Println(doc)
+			// Uncomment to print doc
+			// fmt.Println(doc)
 		}
 		if err := cur.Err(); err != nil {
 			log.Fatal(err)
 		}
-
-		fmt.Println("Running Aggregation Pipeline on taxidata.chicago")
-		collection = client.Database("taxidata").Collection("chicago")
-
-		pipeline := mongo.Pipeline{
-			{{"$match", bson.D{
-				{"trip_seconds", bson.D{{"$gt", 1000}}},
-			}}},
-			{{"$group", bson.D{
-				{"_id", "$payment_type"},
-				{"total_tips", bson.D{{"$sum", "$tips"}}},
-				{"pickup_area", bson.D{{"$addToSet", "$pickup_community_area"}}},
-			}}},
-		}
-		fmt.Println(pipeline)
-		cursor, err := collection.Aggregate(context.Background(), pipeline)
-		defer cursor.Close(context.Background())
-		for cursor.Next(context.Background()) {
-			doc := new(MyAggResult)
-			err := cursor.Decode(doc)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(doc)
-		}
-		fmt.Println("Done.")
-
-		fmt.Println("Sleeping for 2 seconds")
-		time.Sleep(2 * time.Second)
-		iteration += 1
+		time.Sleep(1 * time.Second)
+		collection.DeleteOne(ctx, bson.D{{"x", 1}})
 	}
 }
